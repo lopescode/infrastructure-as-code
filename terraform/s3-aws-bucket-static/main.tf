de999fd3aea6347bb5e9fafa22892e61d6ab32e7
Lopes <leonardolopesb97@hotmail.com>
2024-08-09 15:02:55 -0300

    fix: Fixes for terraform-s3-aws-bucket-static

--- terraform/s3-aws-bucket-static/main.tf
+++ terraform/s3-aws-bucket-static/main.tf
@@ -1,10 +1,9 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # substitua pela região desejada
 }
 
 variable "bucket_name" {
-  description = "The name of the S3 bucket"
-  type        = string
+  type = string
 }
 
 resource "aws_s3_bucket" "static_site_bucket" {
@@ -16,34 +15,33 @@ resource "aws_s3_bucket" "static_site_bucket" {
   }
 
   tags = {
-    Name = "Static Site Bucket"
+    Name        = "Static Site Bucket"
+    Environment = "Production"
   }
 }
 
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
   bucket = aws_s3_bucket.static_site_bucket.id
-
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_acl" "static_site_bucket" {
-  bucket = aws_s3_bucket.static_site_bucket.id
-  acl = "public-read"
-
-  depends_on = [ 
-    aws_s3_bucket_public_access_block.static_site_bucket,
-    aws_s3_bucket_ownership_controls.static_site_bucket,
+  depends_on = [
+	aws_s3_bucket_public_access_block.static_site_bucket,
+	aws_s3_bucket_ownership_controls.static_site_bucket,
   ]
-}
 
+  bucket = aws_s3_bucket.static_site_bucket.id
+  acl    = "public-read"
+}
